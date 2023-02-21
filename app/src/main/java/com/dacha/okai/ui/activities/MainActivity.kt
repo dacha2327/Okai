50f03c84d6b056840b50b5a6c13b41425931f40e
dacha2327 <dachabas2327@gmail.com>
2023-02-21 20:50:25 +0600

    initial commit

---
+++ app/src/main/java/com/dacha/okai/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dacha.okai.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dacha.okai.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
