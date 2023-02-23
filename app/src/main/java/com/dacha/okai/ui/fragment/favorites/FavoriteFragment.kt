4ddf46ec29a6e82d46fb50d7d7b0e549679d67df
dacha2327 <dachabas2327@gmail.com>
2023-02-23 18:00:03 +0600

    UI is created

--- app/src/main/java/com/dacha/okai/ui/fragment/favorites/FavoriteFragment.kt
+++ app/src/main/java/com/dacha/okai/ui/fragment/favorites/FavoriteFragment.kt
@@ -12,11 +12,6 @@ import com.dacha.okai.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FavoriteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FavoriteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
