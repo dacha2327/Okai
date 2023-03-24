83bfa2a22877f23750e7e0f15851c1c9b926a12a
NPaugust <avgust1ne.klaus@gmail.com>
2023-03-24 19:59:45 +0600

    Basket interface

--- app/src/main/java/com/dacha/okai/presentation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/dacha/okai/presentation/ui/activities/MainActivity.kt
@@ -3,10 +3,14 @@ package com.dacha.okai.presentation.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dacha.okai.R
+import com.dacha.okai.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
--- app/src/main/java/com/dacha/okai/presentation/ui/fragment/basket/BasketBlankFragment.kt
+++ app/src/main/java/com/dacha/okai/presentation/ui/fragment/basket/BasketBlankFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.dacha.okai.R
 
 // TODO: Rename parameter arguments, choose names that match
